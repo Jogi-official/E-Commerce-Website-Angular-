de9546dc996f66741b411f6f4c3860c3e6729fb0
AkashOncehub <akash.joshi@oncehub.com>
2023-07-04 12:36:05 +0530

    Added Form with Seller Service and added a JSON server

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { Signup } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent {
+  constructor(private seller : SellerService, private router : Router ){}
 
+  ngOnInit() :void {}
+
+  signUp(data : Signup) : void{
+    this.seller.userSignup(data).subscribe((result)=>{
+      if(result){
+        this.router.navigate(['seller-home']);
+      }
+    });
+  }
 }
