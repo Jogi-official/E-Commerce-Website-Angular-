0ecfe962ee201f885e0bb4d47b499a62030be604
AkashOncehub <akash.joshi@oncehub.com>
2023-07-04 13:15:04 +0530

    Added Authentication Gaurds and Added functionality with the help of local storage

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Signup } from '../data-type';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SellerService {
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor(private http : HttpClient) {}
+  constructor(private http: HttpClient , private router : Router) {}
 
-  userSignup(data:Signup){
-    return this.http.post('http://localhost:3000/seller',data);
+  userSignup(data: Signup) {
+    this.http.post('http://localhost:3000/seller', data, {
+      observe: 'response',
+    }).
+      subscribe((result) => {
+        this.isSellerLoggedIn.next(true);
+        localStorage.setItem('seller-home', JSON.stringify(result.body));
+        this.router.navigate(['seller-home']);
+      });
   }
 
-
-
-
+  reloadSeller(){
+    const localStorageValue = localStorage.getItem('seller-home')
+    if(localStorageValue){
+      this.isSellerLoggedIn.next(true);
+      this.router.navigate(['seller-home']);
+    }
+  }
 
 }
